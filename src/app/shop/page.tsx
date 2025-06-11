04f8ce9f26a7c1ebbafc4cc95cc23d574ea4aa63
nezzyyoung <nezzybucumi@gmail.com>
2025-06-11 11:11:15 +0300

    updates

--- src/app/shop/page.tsx
+++ src/app/shop/page.tsx
@@ -57,7 +57,7 @@ export default function Shop() {
   return (
     <div className="container mx-auto p-4 mt-20">
       <h1 className="text-3xl font-bold mb-6">Shop Page</h1>
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-4 gap-6">
         {products.map((product, index) => (
           <ProductCard
             key={index}
